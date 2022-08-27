631ee909c505a8ce85f298694f3aafa1edb4408b
hausito <106822688+hausito@users.noreply.github.com>
2022-08-27 14:40:45 +0300

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -32,11 +32,10 @@ urlpatterns = [
     re_path(r'^special/',views.special,name='special'),
     re_path(r'^blog/',include('blog.urls')),
     re_path(r'^logout/', views.user_logout, name='logout'),
-
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
    
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-#if settings.DEBUG:
-    #urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
