072ee43aaec0ee1a269772ee54a30892cf2fb64c
hausito <106822688+hausito@users.noreply.github.com>
2022-08-27 14:56:57 +0300

    Update settings.py

--- myapp/settings.py
+++ myapp/settings.py
@@ -30,7 +30,6 @@ SECRET_KEY = 'django-insecure-v(70=2r7vn!#)5po57ojjyd*rw3^$*p45=y_q=3^#+62+czw&t
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    '192.168.100.3',
     '127.0.0.1',
     'utopiablogs.herokuapp.com/',
 ]
